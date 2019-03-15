45396da4b0477fa47d281e9bf4290908f3a107da
Krzysztof Gabara <gabara.krzysztof@gmail.com>
2019-03-15 15:14:27 +0100

    Initial commit

---
+++ src/main/java/com/gabara/complaintservices/ComplaintServicesApplication.java
@@ -0,0 +1,20 @@
+package com.gabara.complaintservices;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ComplaintServicesApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "true");
+        SpringApplication.run(ComplaintServicesApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/gabara/complaintservices/ComplaintServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gabara.complaintservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComplaintServicesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
